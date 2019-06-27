074b677b4d427152c53924578ec48d293c94c86a
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-06-27 08:57:37 -0500

    first commit

---
+++ scripts/3_creandousuarios.R
@@ -0,0 +1,20 @@
+library(mongolite)
+
+mongourl <- "mongodb://<USER>:<PASSWORD>@test..."
+
+personas_con <- mongo("personas_", url = mongourl)
+personas_con$insert('{"name" : "blanca"}')
+personas_con$find(query = '{}', fields = '{}')
+str <- c('{"name" : "jerry"}' , '{"name": "anna", "age" : 23}', '{"name": "joe"}')
+personas_con$insert(str)
+personas_con$find(query = '{}', fields = '{}')
+
+
+{
+nombre: "Miguel",
+edad: 28,
+nacimiento: {
+	pais: "CO",
+	ciudad: "Bogota"
+	}},
+}
